768f4265a6d47b3454f47745479b1947f07a1c5b
Cloud User <ec2-user@ip-172-31-93-102.ec2.internal>
2022-06-01 02:32:42 +0000

    new file added

---
+++ gitProtocols.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+<<mls
+Bank of America has contacted us to make a script to run during its ATM banking transactions. Client is required to provide personal information to authenticate
+mls
+echo "User must verify name, pin and date of birth"
+echo "Welcome to Bank of America. A bank that provides a unique customer experience to meet your individual needs."
+echo "Please enter your first name"
+read firstName
+sleep 1
+echo "Please enter your last name"
+read lastname
+sleep 1
+echo "Please enter your security pin"
+read p
+if [ $p == 0099 ]
+then
+echo "Welcome Bank of America Customer"
+else
+echo "Sorry unable to access your accounts at this time. Please contact customerservice at 1800BANKUSA"
+fi
